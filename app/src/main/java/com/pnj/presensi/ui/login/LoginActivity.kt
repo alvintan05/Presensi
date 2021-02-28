ac73830c82ff77cf09d402b2721636fa79cf3866
alvintan05 <alvintandiardi@gmail.com>
2021-02-28 22:04:37 +0700

    Add Coroutines and Splash Screen

---
+++ app/src/main/java/com/pnj/presensi/ui/login/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.pnj.presensi.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pnj.presensi.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
---
+++ app/src/main/java/com/pnj/presensi/ui/splash/SplashScreenActivity.kt
@@ -0,0 +1,47 @@
+package com.pnj.presensi.ui.splash
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.pnj.presensi.databinding.ActivitySplashScreenBinding
+import com.pnj.presensi.ui.login.LoginActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setFullScreen()
+
+        // coroutine to hold screen several second
+        lifecycleScope.launch {
+            delay(3000)
+            startActivity(Intent(this@SplashScreenActivity, LoginActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun setFullScreen() {
+        // Set full screen splash screen
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+}
