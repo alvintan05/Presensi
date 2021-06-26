5b5a14852524bb5932ed21ce9e001f277a7486c9
Alvin Tandiardi <alvintandiardi@gmail.com>
2021-06-26 14:38:18 +0700

    Change header config in retrofit server

--- app/src/main/java/com/pnj/presensi/network/RetrofitServer.kt
+++ app/src/main/java/com/pnj/presensi/network/RetrofitServer.kt
@@ -3,28 +3,31 @@ package com.pnj.presensi.network
 import com.pnj.presensi.utils.Constant
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.net.ssl.SSLSocketFactory
 
 object RetrofitServer {
 
     private val loggingInterceptor: HttpLoggingInterceptor =
         HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
-    private val okHttpClient: OkHttpClient =
-        OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
-            .build()
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
 
     private val okHttpClientAzure = OkHttpClient.Builder().apply {
-        addInterceptor(
-            Interceptor { chain ->
-                val builder = chain.request().newBuilder()
-                builder.header("Ocp-apim-subscription-key", Constant.API_KEY_AZURE)
-                return@Interceptor chain.proceed(builder.build())
+        addInterceptor(object : Interceptor {
+            override fun intercept(chain: Interceptor.Chain): Response {
+                val request = chain.request()
+                val newRequest = request.newBuilder()
+                    .header("Ocp-apim-subscription-key", Constant.API_KEY_AZURE)
+                    .build()
+                return chain.proceed(newRequest)
             }
-        )
+        })
         addInterceptor(loggingInterceptor)
     }.build()
 
